2b7ab40b5d76821d0d231e29633d67f898e46e1f
Kirill <nikodakirill386@gmail.com>
2021-06-23 18:55:49 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+import "./index.css";
 
 function App() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://balances.vercel.app/api/getHighestBalance")
+      .then((res) => {
+        setData(res.data);
+        console.log(data);
+      });
+  }, [data]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table border="1">
+        <caption>TVL Rates</caption>
+        <tr className="my-tr">
+          <th>DAO Name</th>
+          <th>TVL</th>
+        </tr>
+        {data.map((data) => {
+          return (
+            <tr>
+              <td>{data.name}</td>
+              <td>{data.usd_value}$</td>
+            </tr>
+          );
+        })}
+      </table>
     </div>
   );
 }
